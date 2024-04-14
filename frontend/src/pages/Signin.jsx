63b05ced4cd81f23507512ddc55e17d4dd53366f
Nikhil <72139039+nikhilkoshta@users.noreply.github.com>
2024-04-14 20:17:13 +0530

    Update Signin.jsx

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -7,6 +7,10 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom"
 
 export const Signin = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+    
     return <div className="bg-slate-300 h-screen flex justify-center">
     <div className="flex flex-col justify-center">
       <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
