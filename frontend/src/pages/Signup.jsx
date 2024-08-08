6b3d300003bf65276b625938328e0f03a774f7be
Nikhil <72139039+nikhilkoshta@users.noreply.github.com>
2024-08-08 12:28:12 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -25,7 +25,7 @@ export const Signup = () => {
                 <InputBox onChange={e => setPassword(e.target.value)} placeholder="123456" label={"Password"} />
                 <div className="pt-4">
                     <Button onClick={async () => {
-                        const response = await axios.post("http://localhost:3000/api/v1/user/signup", {
+                        const response = await axios.post("https://pay-money.vercel.app/api/v1/user/signup", {
                             firstName,
                             lastName,
                             username,
