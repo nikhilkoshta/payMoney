2dd316c6b66aa2a967b7934d5d2119ec90acb5a9
Nikhil <nikhilkoshta2@gmail.com>
2024-11-04 16:16:48 +0530

    new changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:3000"
+      "/api/v1": "https://pay-money.vercel.app"
     },
   },
   plugins: [react()],
